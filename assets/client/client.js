e8268e9026973e024073f69a8b772cf6362e07f8
Anxium <axel.avx@gmail.com>
2019-02-22 11:45:03 +0100

    pa

--- assets/client/client.js
+++ assets/client/client.js
@@ -1,11 +1,25 @@
+const config = {
+    apiKey: "AIzaSyDmn75cYckDBvvm41IYEjc79-TPr_1s5tI",
+    authDomain: "messenger-node.firebaseapp.com",
+    databaseURL: "https://messenger-node.firebaseio.com",
+    projectId: "messenger-node",
+    storageBucket: "messenger-node.appspot.com",
+    messagingSenderId: "583194645893"
+};
+firebase.initializeApp(config);
+
+const db = firebase.firestore();
+
 // Récupération des données stockées dans le localStorage
 let usernameGit = window.localStorage.getItem('username')
 
-if(usernameGit === null) {
+if (usernameGit === null) {
     window.location = '/'
 }
 
 const socket = io()
+
+const d = new Date()
 
 // Fonction pour scroll au bas de la liste à chaque message
 const ul = document.querySelector('#messages')
@@ -17,19 +31,49 @@ const scrollMsg = () => {
 const m = document.querySelector('#m')
 document.querySelector('#chat').addEventListener('submit', e => {
     e.preventDefault()
-    socket.emit('chat message', m.value)
-    m.value = ''
+
+    m.value = m.value.trim()
+    if(m.value != '' && m.value.length > 1) {
+        socket.emit('chat message', m.value)
+    
+        db.collection("usersMessages").add({
+            user: usernameGit,
+            msg: m.value,
+            timestamp: d.getTime()
+        })
+        .then(docRef => {
+            console.log("Document written with ID: ", docRef.id)
+        })
+        .catch(error => {
+            console.error("Error adding document: ", error)
+        })
+        
+        m.value = ''
+    } else {
+        alert('Votre message ne doit pas être vide ou doit faire plus d\'un caractère')
+    }
+
     return false;
 })
 
 // Emet la connection d'un utilisateur au serveur avec son pseudo Git
 socket.on('connect', () => {
     socket.emit('new user', usernameGit)
+
+    db.collection("usersMessages").get()
+    .then(querySnapshot => {
+        querySnapshot.forEach((doc) => {
+            const el = document.createElement('li')
+            el.innerText = doc.data().sendTime + ' | ' + doc.data().user + ' -> ' + doc.data().msg
+            document.querySelector('#messages').appendChild(el)
+            scrollMsg() // Appel de la fonction scroll
+        })
+    })
+
 })
 
 // Fonctions qui se lancent lors de la réception d'un event quelconque
 socket.on('chat message', data => {
-    const d = new Date()
     const el = document.createElement('li')
     el.innerText = d.getHours() + 'h' + d.getMinutes() + ' | ' + data.user + ' -> ' + data.msg
     document.querySelector('#messages').appendChild(el)
@@ -41,9 +85,9 @@ socket.on('new user', username => {
     el.innerText = `${username} s'est connecté`
     document.querySelector('#messages').appendChild(el);
 
-    const online = document.createElement('li')
-    online.innerText = username
-    document.querySelector('#onlines').appendChild(online)
+    // const online = document.createElement('li')
+    // online.innerText = username
+    // document.querySelector('#onlines').appendChild(online)
 })
 
 socket.on('disconnect', username => {
